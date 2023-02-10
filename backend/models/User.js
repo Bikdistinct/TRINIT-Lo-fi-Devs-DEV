1a06fbde84d605788a6cf599f3c49cf1cb2c3ddf
bikdistinct <bikdev07@gmail.com>
2023-02-11 02:28:55 +0530

    backend setup

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const mongoose=require("mongoose");
+
+const UserSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    userType:{
+        type:String,
+        default:'farmer'
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number
+    },
+
+    date:{
+        type:Date,
+        default:Date.now
+    },
+
+});
+
+module.exports=mongoose.model("user",UserSchema);
