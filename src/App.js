3dffcc7d4743a846caef80de7447fe4065dfbaba
bikdistinct <bikdev07@gmail.com>
2023-02-11 11:19:24 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, {useState} from 'react';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Landing from './components/UI/Landing';
-
+import Error404 from './pages/Error404';
 const App = () => {
   return (
     <div>
-      <Landing />
+      <Router>
+        <Routes>
+      <Route path="/" element={<Landing/>}/>
+      <Route path="*" element={<Error404/>}/>
+      </Routes>
+      </Router>
+     
     </div>
   );
 }
