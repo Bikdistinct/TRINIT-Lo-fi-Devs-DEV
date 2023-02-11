720df2ff0e19b419cae6525c3295e35c21c48e7e
ritz <sritwick10@gmail.com>
2023-02-11 21:41:58 +0530

    edits in feed

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,17 +1,21 @@
 import Post from "../components/post/post";
-// import Share from "../share/Share";
+import Share from "../components/post/Share";
 import "./Feed.css";
 import { Posts } from "../dummyData";
+import Navbar from '../components/UI/Navbar';
 
 export default function Feed() {
   return (
-    <div className="feed">
-      <div className="feedWrapper">
-        {/* <Share /> */}
-        {Posts.map((p) => (
-          <Post key={p.id} post={p} />
-        ))}
+    <>
+      <Navbar />
+      <div className="feed">
+        <div className="feedWrapper">
+          <Share />
+          {Posts.map((p) => (
+            <Post key={p.id} post={p} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
