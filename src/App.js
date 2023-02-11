fc0bdbf12507ac788c5ad9c4a6f4414e2d22e283
bikdistinct <bikdev07@gmail.com>
2023-02-11 11:39:34 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, {useState} from 'react';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Landing from './components/UI/Landing';
 import Error404 from './pages/Error404';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
       <Route path="/" element={<Landing/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Signup/>}/>
       <Route path="*" element={<Error404/>}/>
+
       </Routes>
       </Router>
      
